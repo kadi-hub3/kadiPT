e32d05f785cea6053cce4a1723b2cf79145ed761
Kadi M <kicktheball1@gmail.com>
2021-11-11 20:27:38 +0100

    domain

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router exact path='/'>
     <App />
     </Router>
   </React.StrictMode>,
